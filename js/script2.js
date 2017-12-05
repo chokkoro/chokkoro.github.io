5196f2b5b56841d0ad10a2fd5231883e4341b763
chokkoro <34284937+chokkoro@users.noreply.github.com>
2017-12-05 21:13:40 +0100

    first website upload

---
+++ js/script2.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+$(window).scroll(function(event){
+
+ var y = $(this).scrollTop();
+
+ if(y >=1000){
+
+$('.map_mobil2').addClass('animate');
+$('.map_mobil3').addClass('animate');
+
+
+ }
+
+
+});
+
+});
