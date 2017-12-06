41f071cc3dea578f19e8dd1ce7bd6c3d88061ea6
chokkoro <34284937+chokkoro@users.noreply.github.com>
2017-12-07 00:24:34 +0100

    w

--- js/script2.js
+++ js/script2.js
@@ -4,8 +4,8 @@ $(window).scroll(function(event){
 
  var y = $(this).scrollTop();
 
- if(y >=1000){
-
+ if(y >=1500){
+$('.map_mobil').addClass('animate');
 $('.map_mobil2').addClass('animate');
 $('.map_mobil3').addClass('animate');
 
